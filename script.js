1c518a01b856066686ed03721efcac601cae7f67
sbmco05 <sofia.mirandacouto@gmail.com>
2025-03-13 12:50:04 +0000

    Starting portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const textElement = document.getElementById("text");
+    const text = "> loading portfolio";
+    let i = 0;
+
+    function effectWrite() {
+        if (i < text.length) {
+            textElement.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(effectWrite, 75); //Write effect
+        }
+    }
+
+    effectWrite();
+});
