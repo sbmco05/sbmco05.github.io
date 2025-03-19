bb9b6061406af6694d4d483046a1b463c76300b2
sbmco05 <sofia.mirandacouto@gmail.com>
2025-03-19 12:16:50 +0000

    Experiment

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const text = "> loading";
     let i = 0;
 
+    setTimeout(effectWrite, 1000);
+
     function effectWrite() {
         if (i < text.length) {
             textElement.innerHTML += text.charAt(i);
             i++;
-            setTimeout(effectWrite, 75); //Write effect
+            setTimeout(effectWrite, 100); //Write effect
         }
     }
 
