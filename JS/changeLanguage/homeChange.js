897ce6c62f5c5be0b2012f508c909c347780dedd
sbmco05 <sofia.mirandacouto@gmail.com>
2025-03-25 18:56:59 +0000

    Translation changes

--- JS/changeLanguage/homeChange.js
+++
@@ -1,8 +0,0 @@
-var textPT = document.querySelector('.textContainerPT');
-var textEN = document.querySelector('.textContainer');
-var btnTranslate = document.querySelector('#btnTranslate');
-
-btnTranslate.addEventListener('click', function(){
-    textPT.classList.toggle('active')
-    textEN.classList.toggle('active')
-})
